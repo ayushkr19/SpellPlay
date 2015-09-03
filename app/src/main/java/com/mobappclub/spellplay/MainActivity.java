6c92ddcf24592f8822ed94e21af633823cd52496
Ayush Kumar <ayushkr19@gmail.com>
2015-09-03 11:54:13 +0530

    Change UI of app

--- app/src/main/java/com/mobappclub/spellplay/MainActivity.java
+++ app/src/main/java/com/mobappclub/spellplay/MainActivity.java
@@ -1,8 +1,10 @@
 package com.mobappclub.spellplay;
 
 import android.content.Intent;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -12,6 +14,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setupToolbar();
+    }
+
+    private void setupToolbar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setHomeAsUpIndicator(R.mipmap.ic_launcher);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
     }
 
 
